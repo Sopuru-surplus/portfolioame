d09491bc3d0df0d02ce092e810a87e993b181cbf
Sopuru-surplus <74392013+Sopuru-surplus@users.noreply.github.com>
2021-10-28 06:05:39 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+var btn = document.getElementsByClassName("navbar-toggler");
+var collapse = document.getElementById("navbarCollapse");
+
+function hideDis(){
+    if (condition) {
+        
+    }
+}
